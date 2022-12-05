51365e6f698d9bc128655199878749600db24df0
aryanathan <104875633+aryanathan@users.noreply.github.com>
2022-12-04 16:52:30 -0800

    Update Die challengePart1.java

--- Die challengePart1.java
+++ Die challengePart1.java
@@ -23,7 +23,53 @@ public class Die {
 
 
     }
- 
+
+
+
+
+____________________________
+
+public class Die{
+private int numberOfSides;
+private String color;
+private int faceValue;
+
+//initalizes the number of sides on a Die as 6
+public int Die(){
+    numberOfSides=6;
+}
+//initalizes the number of sides as N
+public  int Die(int N){
+    numberOfSides=N;
+}
+//returns the number of sides on a die
+public  int numSides(){
+    return numberOfSides;
+}
+//rolls die 
+public roll(){
+    faceValue= (int)(Math.random()*numberOfSides)+1;
+    return faceValue;
+}
+ //returns the color of the die
+public static String dieColor(){
+    return color;
+}
+
+
+
+
+
+
+
+
+________________________
+public class DieTester{
+    public static void main(String args[]){
+    
+int n =1000;
+
+  
     
     
     
